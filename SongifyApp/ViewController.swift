e9da52b8e89ce53210292ee119baa8a67c1f01c2
Matthew Piedra <matthewluispiedra@gmail.com>
2021-11-08 20:22:55 -0500

    Add app icon and splash screen

---
+++ SongifyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Songify
+//
+//  Created by Matthew Piedra on 11/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

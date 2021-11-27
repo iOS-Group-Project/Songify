b34c7bb01e9f6c1b7324dd5a278ddac8c7941ba3
Matthew Piedra <matthewluispiedra@gmail.com>
2021-11-27 13:24:58 -0500

    add new track list file

---
+++ SongifyApp/TrackListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrackListViewController.swift
+//  Songify
+//
+//  Created by Matthew Piedra on 11/27/21.
+//
+
+import UIKit
+
+class TrackListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

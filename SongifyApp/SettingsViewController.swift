25c3dbf0ded054f530dfd3263c4e1029c5cf943f
Matthew Piedra <66268282+matthewpiedra@users.noreply.github.com>
2021-12-23 12:44:02 -0500

    Update settings options

--- SongifyApp/SettingsViewController.swift
+++ SongifyApp/SettingsViewController.swift
@@ -9,33 +9,18 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-    @IBOutlet weak var sliderNumber: UILabel!
     @IBOutlet weak var groupSegment: UISegmentedControl!
-    @IBOutlet weak var popSlider: UISlider!
     @IBOutlet weak var alphabetToggle: UISwitch!
-    @IBOutlet weak var popularityToggle: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additiona!l setup after loading the view.
-        let selectedPopLevel = UserDefaults.standard.string(forKey: "popularityLevel")
         let selectedSegIdx = UserDefaults.standard.integer(forKey: "selectedSegIdx")
-        let sortByPopularity = UserDefaults.standard.bool(forKey: "sortByPopularity")
         let sortByAlphabet = UserDefaults.standard.bool(forKey: "sortByAlphabet")
         
-        if selectedPopLevel != nil {
-            popSlider.value = Float(selectedPopLevel!)!
-        }
         groupSegment.selectedSegmentIndex = selectedSegIdx
         alphabetToggle.isOn = sortByAlphabet
-        popularityToggle.isOn = sortByPopularity
-    }
-    
-    @IBAction func slider(_ sender: UISlider) {
-        let value = Int(sender.value)
-        sliderNumber.text = String(value)
-        UserDefaults.standard.set(sliderNumber.text, forKey: "popularityLevel")
     }
     
     @IBAction func selectedGroup(_ sender: UISegmentedControl) {
@@ -43,12 +28,6 @@ class SettingsViewController: UIViewController {
         let title = sender.titleForSegment(at: idx)
         UserDefaults.standard.set(idx, forKey: "selectedSegIdx")
         UserDefaults.standard.set(title, forKey: "albumGroup")
-    }
-    
-    @IBAction func togglePopularity(_ sender: UISwitch) {
-        // sender.isOn
-        let isOn = sender.isOn
-        UserDefaults.standard.set(isOn, forKey: "sortByPopularity")
     }
     
     @IBAction func toggleAlphabet(_ sender: UISwitch) {
